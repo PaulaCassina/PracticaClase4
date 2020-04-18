8736a1177d124823f0686b3d93794382820c369d
PaulaCassina <paulacassina@hotmail.com>
2020-04-17 21:28:51 -0300

    Resolucion puntos del 1 al 5

---
+++ Practica4/Logica/Extensiones.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Logica
+{
+    public static class Extensiones
+    {
+        public static int RetornarCantidadPalabras(this string cadenastring)
+        {
+            string[] arraypalabras = cadenastring.Split(' ');
+            return arraypalabras.Count();
+            
+        }
+
+        public static int RetornarCantidadPalabras(this string cadenastring, char separador)
+        {
+            string[] arraypalabras = cadenastring.Split(separador);
+            return arraypalabras.Count();
+        }
+
+        public static decimal ObtenerMinutosDiferenciaFechas(this DateTime fechamayor, DateTime fechamenor)
+        {
+            TimeSpan diferenciaFechas = fechamayor - fechamenor;
+            double diferenciaensegundos = diferenciaFechas.TotalSeconds;
+            return Convert.ToDecimal(diferenciaensegundos/60);
+        }
+
+        public static decimal ObtenerPromedio(this List<int> listaenteros)
+        {
+            decimal total =Convert.ToDecimal(listaenteros.Average());
+            return total;
+        }
+
+        public static int ObtenerMultiplicacionParesImpares(this List<int> listaenteros)
+        {
+            int multiplicacion = (listaenteros.FindAll(x => x % 2 == 0).Sum())*((listaenteros.First(x=>x%2!=0)*2)+((listaenteros.FindAll(x=>x%2!=0).Sum()*-1)));
+
+            return multiplicacion;
+        }
+    }
+}
